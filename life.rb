1d0fd3502f3caab12864d04e1a20108a9f6c1be8
Wally Glutton <stungeye@gmail.com>
2011-05-10 09:47:55 -0500

    Refactored before block for all specs. Completed neighbour specs.

--- life.rb
+++ life.rb
@@ -26,7 +26,16 @@ class Life
 
   def neighbours x, y
     raise OutOfBoundsError  if out_of_bounds x, y
-    0
+    neighbours_found = 0
+    @cells.each do |cx, cy|
+      neighbours_found += 1  if neighbour? cx, cy, x, y
+    end
+    neighbours_found
+  end
+
+  def neighbour? x1, y1, x2, y2
+    return false  if x1 == x2 && y1 == y2
+    (x1 - x2).abs <= 1 && (y1 - y2).abs <= 1
   end
 
   def out_of_bounds x, y
--- test_life.rb
+++ test_life.rb
@@ -2,70 +2,88 @@ require 'minitest/autorun'
 require_relative 'life'
 
 describe Life do
+  before do
+    @empty_cells = []
+    @empty_life  = Life.new @empty_cells, 5, 5 
+
+    # Crowded Cells:
+    # *--**
+    # ---**
+    # -***-
+    # ****-
+    # *****
+    @crowded_cells = [[0,0], [3,0], [4,0],
+                      [3,1], [4,1],
+                      [1,2], [2,2], [3,2],
+                      [0,3], [1,3], [2,3], [3,3],
+                      [0,4], [1,4], [2,4], [3,4], [4,4]]
+    @crowded_life = Life.new @crowded_cells, 5, 5
+
+    # Lonely Cells:
+    # *-*-*
+    # -----
+    # *-*-*
+    # -----
+    # *-*-*
+    @lonely_cells = [[0,0], [2,0], [4,0],
+                     [0,2], [2,2], [4,2],
+                     [0,4], [2,4], [4,4]]
+    @lonely_life = Life.new @lonely_cells, 5, 5
+  end
 
   describe "when asked for its class" do
     it "must response with Life" do
-      @life = Life.new [], 5, 5
-      @life.class.must_equal Life
+      @empty_life.class.must_equal Life
     end
   end
 
   describe "when initialized with an empty array" do
-    before do
-      @cells = []
-      @life = Life.new @cells, 5, 5
-    end
     it "must contain an empty cells array" do
-      @life.cells.must_equal @cells
+      @empty_life.cells.must_equal @empty_cells
     end
     it "must be able to to_s these cells to the correct string" do
       output_string = "-----\n" * 5
-      @life.to_s.must_equal output_string
+      @empty_life.to_s.must_equal output_string
     end
   end
 
   describe "when created with a specific cell array" do
-    before do
-      @cells = [[1,1],[2,2],[2,3]]
-      @life = Life.new @cells, 5, 5
-    end
     it "must contain that same cell array" do
-      @life.cells.must_equal @cells
+      @crowded_life.cells.must_equal @crowded_cells
     end
     it "must be able to to_s these cells to the correct string" do
-      output_string =  "-----\n"
-      output_string << "-*---\n"
-      output_string << "--*--\n"
-      output_string << "--*--\n"
-      output_string << "-----\n"
-      @life.to_s.must_equal output_string
+      output_string =  "*--**\n"
+      output_string << "---**\n"
+      output_string << "-***-\n"
+      output_string << "****-\n"
+      output_string << "*****\n"
+      @crowded_life.to_s.must_equal output_string
     end
   end
 
   describe "when created with only lonely cells" do
     it "must kill off all lonely cells" do
-      @cells = [[0,0], [2,0], [4,0],
-                [0,2], [2,2], [4,2],
-                [0,4], [2,4], [4,4]]
-      @life = Life.new @cells, 5, 5
-      @next_gen = @life.next_gen
+      @next_gen = @lonely_life.next_gen
       @next_gen.cells.must_equal []
     end
   end
 
   describe "when cells have neighbours" do
     #it "must raise an OutOfBounds exception if we request an out of bounds coordinate" do
-    #  @life = Life.new [], 5, 5
-    #  @life.neighbours(-1, -1).must_raise RuntimeError
+    #  @crowded_life.neighbours(-1, -1).must_raise RuntimeError
     #end
     it "must return the correct number of neighbours" do
-      @cells = [[0,0], [3,0], [4,0],
-                       [3,1], [4,1],
-                [1,2], [2,2], [3,2],
-                [1,3], [2,3], [3,3],
-                [1,4], [2,4], [3,4]]
-      @life = Life.new @cells, 5, 5
-      @life.neighbours(0, 0).must_equal 0
+      @crowded_life.neighbours(0, 0).must_equal 0
+      @crowded_life.neighbours(1, 0).must_equal 1
+      @crowded_life.neighbours(2, 0).must_equal 2
+      @crowded_life.neighbours(4, 0).must_equal 3
+      @crowded_life.neighbours(4, 3).must_equal 4
+      @crowded_life.neighbours(2, 1).must_equal 5
+      @crowded_life.neighbours(3, 3).must_equal 6
+      @crowded_life.neighbours(1, 3).must_equal 7
+      @crowded_life.neighbours(2, 3).must_equal 8
     end
   end
 end
+
+
